9a035daa3bb2b6921c8a1021e0356be07f4a38e1
Mohammaduvez <mohammaduvez1995@gmail.com>
2019-03-10 18:52:19 +0530

    Changes in View Files Fragment - Checking PDF encryption status on background thread - Added PDFFile Model to store encryption status of PDF file

--- app/src/main/java/swati4star/createpdf/util/PDFEncryptionUtility.java
+++ app/src/main/java/swati4star/createpdf/util/PDFEncryptionUtility.java
@@ -63,7 +63,8 @@ public class PDFEncryptionUtility {
         mPasswordInput.addTextChangedListener(
                 new TextWatcher() {
                     @Override
-                    public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                    }
 
                     @Override
                     public void onTextChanged(CharSequence s, int start, int before, int count) {
